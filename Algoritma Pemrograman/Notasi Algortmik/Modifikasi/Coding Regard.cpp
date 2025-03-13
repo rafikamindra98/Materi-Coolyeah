fbf6253a70a8ba0494d0a5581d7a248b93cace9f
Rafi Kamindra <116833461+rafikamindra98@users.noreply.github.com>
2025-03-13 14:56:31 +0700

    Add files via upload

---
+++ Algoritma Pemrograman/Notasi Algortmik/Modifikasi/Coding Regard.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+	/*PROGRAM Regard
+	DEKALRASI*/
+	string nama, regard;
+	//ALGORITMA
+	cout<<"Nama=";cin>>nama;
+	regard = "Halo, "+ nama;
+	cout<<regard<<endl;
+	cin.get();
+}
