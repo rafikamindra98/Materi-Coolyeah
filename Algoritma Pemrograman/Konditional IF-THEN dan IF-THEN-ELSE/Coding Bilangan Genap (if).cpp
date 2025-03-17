6af56968180dbc475d5e78029c9fd926beddccbf
Rafi Kamindra <116833461+rafikamindra98@users.noreply.github.com>
2025-03-18 06:06:11 +0700

    Add files via upload

---
+++ Algoritma Pemrograman/Konditional IF-THEN dan IF-THEN-ELSE/Coding Bilangan Genap (if).cpp
@@ -0,0 +1,11 @@
+//PROGRAM MENENTUKAN BILANGAN GENAP IF
+#include <iostream>
+using namespace std;
+int main(){
+	//DEKLARASI
+	int bilangan;
+	//ALGORITMA
+	cout<<"Masukkan suatu bilangan bulat: ";cin>>bilangan;
+	if (bilangan%2==0)
+	cout<<"Bilangan Genap"<<endl;
+}
