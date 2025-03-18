56a1dbcd211093b510d70d4f6e7ffe0c3635328e
Rafi Kamindra <116833461+rafikamindra98@users.noreply.github.com>
2025-03-18 14:00:22 +0700

    Add files via upload

---
+++ Program Modular/Pemrograman Modular/Latihan/Tulis Nama/Unit1.pas
@@ -0,0 +1,46 @@
+unit Unit1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Label1: TLabel;
+    Label2: TLabel;
+    Edit1: TEdit;
+    Label3: TLabel;
+    Edit2: TEdit;
+    Button1: TButton;
+    Button2: TButton;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+  Var nama: string;
+begin
+  nama := Edit1.Text;
+  Edit2.Text:=nama;
+
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+  application.terminate
+end;
+
+end.
